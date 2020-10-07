e33e7954d243302887f8ed625c5926ba932d8dd6
Bulat <bulatkash10@mail.ru>
2020-10-07 22:47:30 +0300

    first commit

---
+++ Git/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sign up");
+            Console.WriteLine("Username:");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine();
+        }
+    }
+}
